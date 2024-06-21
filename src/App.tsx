034eb950132c3eb426364646e5c5f8004578e0fd
Charles <chuckm248@gmail.com>
2024-06-21 13:04:08 -0600

    add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/home";
+import HostAnEvent from "./pages/HostAnEvent/HostAnEvent";
+import HomePage from "./pages/Home/Home";
+import Event from "./pages/Event/Event";
+import NotFound from "./pages/NotFound/NotFound";
+import User from "./pages/User/User";
+import Profile from "./pages/Profile/Profile";
+import ContactUs from "./pages/Contact/ContactUs";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/events">
+        <Route index element={<HomePage />} />
+        <Route path=":id" element={<Event />} />
+        <Route path="host-an-event" element={<HostAnEvent />} />
+      </Route>
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/user/:id" element={<User />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
