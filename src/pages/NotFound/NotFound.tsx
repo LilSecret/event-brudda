2b4388f539a25ce38f02feb9efa6adecbec80db1
Charles <chuckm248@gmail.com>
2024-07-12 16:31:08 -0600

    style and build not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,33 @@
+import Logo from "../../components/Logo/Logo";
 import "./styles.css";
 
 function NotFound() {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <header className="logo">
+        <Logo type="dark" />
+      </header>
+      <main className="main main-inline-mw">
+        <div className="error-img"></div>
+        <div className="content">
+          <h2 className="heading">Not Found (404)</h2>
+          <h3 className="heading">Oops!</h3>
+          <div className="content-text">
+            <p>The page you have entered does not exist.</p>
+
+            <p>
+              Please make your way back to our home page by clicking on the
+              "Brudda" logo above. If you have any questions please{" "}
+              <a className="link" href="/contact-us">
+                Contact Us
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default NotFound;
