714fdc8dddca7dbfc0b3fdbf202e9b3ed03627d3
Charles <chuckm248@gmail.com>
2024-07-12 13:20:03 -0600

    style and structure login and sign up

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,7 +1,35 @@
+import TextInput from "../../components/Form/TextInput";
+import Logo from "../../components/Logo/Logo";
 import "./styles.css";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <header className="logo">
+        <Logo type="dark" />
+      </header>
+      <main>
+        <form action="#" className="auth-form">
+          <h2 className="heading">Sign In</h2>
+          <div className="form-inputs">
+            <TextInput label="User Name" name="username" type="text" />
+            <TextInput label="Password" name="password" type="password" />
+          </div>
+          <div className="form-actions">
+            <button type="submit" className="tag" data-size="full-bleed">
+              Sign In
+            </button>
+            <p className="form-text">
+              New to Brudda?{" "}
+              <a className="link" href="/sign-up">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
 
 export default Login;
--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -1,7 +1,48 @@
+import TextInput from "../../components/Form/TextInput";
+import TextSeparator from "../../components/Form/TextSeparator";
+import Logo from "../../components/Logo/Logo";
 import "./styles.css";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <header className="logo">
+        <Logo type="dark" />
+      </header>
+      <main>
+        <form action="#" className="auth-form">
+          <h2 className="heading">Sign Up</h2>
+          <p>Welcome to Brudda!</p>
+          <div className="form-inputs">
+            <TextInput label="First Name" name="fname" type="text" />
+            <TextInput label="Last Name" name="lname" type="text" />
+            <TextInput label="Email" name="email" type="email" />
+            <TextInput label="Username" name="username" type="text" />
+            <TextInput label="Password" name="password" type="password" />
+            <TextInput
+              label="Confirm Password"
+              name="cpassword"
+              type="password"
+            />
+          </div>
+          <div className="form-actions">
+            <button type="submit" className="tag" data-size="full-bleed">
+              Sign Up
+            </button>
+            <TextSeparator text="Or" />
+            <a
+              href="/login"
+              className="tag"
+              data-type="outline"
+              data-size="full-bleed"
+            >
+              Sign In
+            </a>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
 
 export default SignUp;
