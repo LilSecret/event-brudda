930e046a2bde9f2e43ed346984dd5f62450ff2b3
Charles <chuckm248@gmail.com>
2024-09-03 13:59:27 -0600

    style not found page using css modules

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,23 +1,23 @@
 import Logo from "../../components/Logo/Logo";
-import "./styles.css";
+import styles from "./notfound.module.css";
 
 function NotFound() {
   return (
-    <>
-      <header className="logo">
+    <div className={styles.page}>
+      <header className={styles.logo}>
         <Logo type="dark" />
       </header>
       <main className="even-columns main-inline-mw" data-type="narrow">
-        <div className="error-img"></div>
-        <div className="content">
+        <img className={styles["error-img"]} src="/sad-dog.png" alt="sad dog" />
+        <div className={styles.content}>
           <h2 className="heading">Not Found (404)</h2>
           <h3 className="heading">Oops!</h3>
-          <div className="content-text">
+          <div className={styles["content-text"]}>
             <p>The page you have entered does not exist.</p>
 
             <p>
-              Please make your way back to our home page by clicking on the
-              "Brudda" logo above. If you have any questions please{" "}
+              Please make your way back to our home page by clicking on the "Brudda" logo above. If
+              you have any questions please{" "}
               <a className="link" href="/contact-us">
                 Contact Us
               </a>
@@ -26,7 +26,7 @@ function NotFound() {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
