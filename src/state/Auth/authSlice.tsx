82cd58b625d57511c5aaa7060aeb639dc93229b2
Charles <chuckm248@gmail.com>
2024-09-03 19:31:26 -0600

    remove async code from reducer and rename exported actions

--- src/state/Auth/authSlice.tsx
+++ src/state/Auth/authSlice.tsx
@@ -5,42 +5,27 @@ import toast from "react-hot-toast";
 
 interface AuthState {
   user: TUser | undefined;
+  isSubscribed: boolean;
 }
 
 const initialState: AuthState = {
   user: undefined,
+  isSubscribed: false,
 };
 
 const authSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    login: (
-      state: AuthState,
-      action: PayloadAction<{ username: string; password: string }>
-    ) => {
-      API_USERS.getAllUsers().then((users) => {
-        const { username, password } = action.payload;
-        const user = users.find((user: TUser) => user.username === username);
-        const doesPasswordMatch = user?.password === password;
-
-        if (!user) {
-          toast.error("That user doesn't exist");
-          return;
-        }
-        if (!doesPasswordMatch) {
-          toast.error("Password does not match");
-          return;
-        }
-        toast.success("Login Success 😃");
-        localStorage.setItem("user", JSON.stringify(user));
-        state.user = user;
-        // window.location.replace("http://localhost:5173");
-      });
+    login: (state: AuthState, action: PayloadAction<TUser>) => {
+      state.user = action.payload;
+    },
+    subscribe: (state) => {
+      state.isSubscribed = true;
     },
   },
 });
 
-export const { login } = authSlice.actions;
+export const authActions = authSlice.actions;
 
 export default authSlice.reducer;
